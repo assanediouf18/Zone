aa51eeb922e978c2e26a3cd88edbfd43bf70f8b1
adiouf <h8799r@gmail.com>
2024-06-08 15:53:02 +0200

    Added drawer and chat screen

---
+++ app/(drawer)/index.tsx
@@ -0,0 +1,34 @@
+import { useCallback, useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import { GiftedChat, IMessage } from "react-native-gifted-chat";
+
+export default function Index() {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages: IMessage[] = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <GiftedChat messages={messages}/>
+    </View>
+  );
+}
