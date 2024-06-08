3281320f258c414e06e9130322199e70bf6a57e9
adiouf <h8799r@gmail.com>
2024-06-08 17:44:27 +0200

    Possible to take pictures

--- app/add-photo.tsx
+++ app/add-photo.tsx
@@ -1,10 +1,20 @@
-import { CameraType, CameraView, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import { CameraCapturedPicture, CameraType, CameraView, useCameraPermissions } from "expo-camera";
+import { useRef, useState } from "react";
+import {
+  Button,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function AddPhoto() {
-  const [facing, setFacing] = useState('back');
+  const [facing, setFacing] = useState("back");
   const [permission, requestPermission] = useCameraPermissions();
+
+  let camera = useRef<CameraView>();
 
   if (!permission) {
     return <View />;
@@ -13,22 +23,47 @@ export default function AddPhoto() {
   if (!permission.granted) {
     return (
       <View style={styles.container}>
-        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
         <Button onPress={requestPermission} title="grant permission" />
       </View>
     );
   }
 
   function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
+  function takePicture() {
+    camera.current?.takePictureAsync().then((response: CameraCapturedPicture | undefined) => {
+      if(response) {
+        console.log(response);
+      }
+    })
   }
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing as CameraType}>
+      <CameraView style={styles.camera} facing={facing as CameraType} ref={camera}>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
+          <TouchableOpacity style={[styles.button]} onPress={takePicture}>
+            <Ionicons
+              name="camera"
+              size={64}
+              color={"white"}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, {transform: [{scale: 1/2}]}]} onPress={toggleCameraFacing}>
+            <MaterialIcons
+              name={
+                Platform.OS === "ios"
+                  ? "flip-camera-ios"
+                  : "flip-camera-android"
+              }
+              size={64}
+              color={"white"}
+            />
           </TouchableOpacity>
         </View>
       </CameraView>
@@ -39,25 +74,25 @@ export default function AddPhoto() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   camera: {
     flex: 1,
   },
   buttonContainer: {
     flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
+    flexDirection: "column",
+    backgroundColor: "transparent",
     margin: 64,
+    alignItems: "center"
   },
   button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
+    alignSelf: "flex-end",
+    alignItems: "center",
   },
   text: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
+    fontWeight: "bold",
+    color: "white",
   },
 });
--- components/Chat/ChatActions.tsx
+++ components/Chat/ChatActions.tsx
@@ -11,7 +11,7 @@ interface ChatActionsProps {
 export default function ChatActions(props: ChatActionsProps) {
     return props.show && (
         <View style={{ flex: 1, flexDirection: "row", alignItems: "center", justifyContent: "space-around" }}>
-            <Ionicons name="add" size={32} />
+            {/* <Ionicons name="add" size={32} /> */}
             <Link href={"add-photo"}>
                 <Ionicons name="camera" size={32} />
             </Link>
